356d5ed24c54311d23cad4c5087824131678ac93
theTtay <3874262@clases.edu.sv>
2025-03-04 15:06:22 -0600

    QueryString - segundo commit y push

--- ConfirmProduct.aspx.cs
+++ ConfirmProduct.aspx.cs
@@ -11,7 +11,25 @@ namespace QueryString
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			
+			//Devolviendo los parametros de la cadena de consulta para el codigo URL
+			ddlCategory.SelectedValue = Request.QueryString["ddlCategory"];
+			ddlSupplier.SelectedValue = Request.QueryString["ddlSupplier"];
+			txtProduct.Text = Request.QueryString["strProduct"];
+			txtDescription.Text = Request.QueryString["strDescription"];
+			txtImage.Text = Request.QueryString["strImage"];
+			Decimal decPrice =
+				Convert.ToDecimal(Request.QueryString["decPrice"]);
+			txtPrice.Text = decPrice.ToString("c");
+			txtNumberInStock.Text = Request.QueryString["bytNumberInStock"];
+			txtNumberOnOrder.Text = Request.QueryString["bytNumberOnOrder"];
+			txtReorderLevel.Text = Request.QueryString["bytReorderLevel"];
+			//Compute and display the value in stock and the value in stock and the value on order
+			Byte bytNumberInStock = Convert.ToByte(Request.QueryString["bytNumberInStock"]);
+			Byte bytNumberOnOrder = Convert.ToByte(Request.QueryString["bytNumberOnOrder"]);
+			Decimal decValueInStock = decPrice * bytNumberInStock;
+			Decimal decValueOnOrder = decPrice * bytNumberOnOrder;
+			txtNumberInStock.Text = decValueInStock.ToString("c");
+			txtNumberOnOrder.Text = decValueOnOrder.ToString("c");
 		}
 
 		protected void btnSave_Click(object sender, EventArgs e)
