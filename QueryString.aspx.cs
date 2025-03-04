82fed35ca3b2f92725a3744ff2a55f337d5118eb
theTtay <3874262@clases.edu.sv>
2025-03-04 14:31:39 -0600

    QueryString - primer commit y push

---
+++ QueryString.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QueryString
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnConfirm_Click(object sender, EventArgs e)
+		{
+			
+        }
+
+		protected void btnConfirm_Click1(object sender, EventArgs e)
+		{
+			//Go to the confirmation page with the encoded URL
+			Response.Redirect
+				(
+					"ConfirmProduct.aspx" +
+					"?ddlCategory=" + ddlCategory.SelectedValue +
+					"&ddlSupplier=" + ddlSupplier.SelectedValue +
+					"&strProduct=" + txtProduct.Text +
+					"&strDescription=" + txtDescription.Text +
+					"&strImage=" + txtImage.Text +
+					"&decPrice=" + txtPrice.Text +
+					"&bytNumberInStock=" + txtNumberInStock.Text +
+					"&bytNumberOnOrder=" + txtNumberOnOrder.Text +
+					"&bytReorderLevel=" + txtReorderLevel.Text
+				);
+		}
+	}
+}
